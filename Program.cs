a4868cc23300721504ca1cffa1c92a28f7ab96d2
Roman Kutsenko <73640616+KutsenkoRN@users.noreply.github.com>
2023-08-10 21:59:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace H_W_Mod3_5;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var concatenatedText = await ConcatenateTextAsync();
+        Console.WriteLine(concatenatedText);
+    }
+
+    static async Task<string> ReadFileAsync(string filename)
+    {
+        using (var reader = new StreamReader(filename))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+
+    static async Task<string> ConcatenateTextAsync()
+    {
+        var helloTask = ReadFileAsync("Hello.txt");
+        var worldTask = ReadFileAsync("World.txt");
+
+        return $"{await helloTask}{await worldTask}";
+    }
+}
